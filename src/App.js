c41adc8564bbf576f72815785d4bc183fa3db670
Matias Ramirez <matias_e9@hotmail.com>
2020-04-21 01:40:26 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Menu from './components/Menu/Menu';
+import Background from './components/Background/Background';
+import SideMenu from './components/SideMenu/SideMenu';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
+  const [sideMenuOpen, setMenuState] = useState(false);
+    
+  const menuToggleClickHandler = () => {
+    setMenuState(!sideMenuOpen)
+  };
+  
+  const backStateClickHandler = () => {
+    setMenuState(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Menu clickHandler={menuToggleClickHandler}/>
+        {sideMenuOpen ? <Background click={backStateClickHandler}/> : ''}
+        <SideMenu show={sideMenuOpen} remove={backStateClickHandler}/>
+
+        <div className="container">
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+        </div>
+      </Router>
+      
     </div>
   );
 }
 
+const Home = () => {
+  return (
+    <div className="Home">
+      <p>Home</p>
+    </div>
+  );
+};
+
+const About = () => (
+  <div className="About">
+    <p>About</p>
+  </div>
+);
 export default App;
